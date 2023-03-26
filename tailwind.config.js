861afaf6179b8bf9fbc45462a4659e741ac73dcd
Shivam Kumar <shivkumar386112@gmail.com>
2023-03-26 17:12:45 +0530

    initialized next.js project with tailwindcss and installed all the dependencies like moment graphql graphql-request html-react-parser react-multi-carousel

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    //If you're using the src/ directory
+  "./src/**/*.{js,ts,jsx,tsx}",
+
+    //If you're using the default paths:
+  "./app/**/*.{js,ts,jsx,tsx}",
+  "./pages/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",
+
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
